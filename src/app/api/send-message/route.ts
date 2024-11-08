6affa39f9c9515ebff108e5a30e3f31d34ae4116
divPatel <divpatel4081@gmail.com>
2024-11-09 00:33:22 +0600

    post route for send message to user

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,29 @@
+import UserModel, { Message } from "@/model/User";
+import { NextResponse } from "next/server";
+
+export async function POST(req:Request){
+    try {
+        const {username , content} = await req.json();
+        const user = await UserModel.findOne({username});
+        if(!user || !user.isAcceptingMessage){
+            return NextResponse.json({
+                success:false,
+                message:"user not found or user not accepting messages"
+            },{status:404})
+        }
+        const newMessage = {content , createdAt:new Date()} as Message;
+        user.message.push(newMessage);
+
+        return NextResponse.json({
+            success:true,
+            message:"message sent succesfully",
+        } , {status:200})
+
+    } catch (error) {
+        console.log("oops not able to send message",error);
+        return NextResponse.json({
+            success:false,
+            message:"not able to send message",
+        } , {status:401})
+    }
+}
